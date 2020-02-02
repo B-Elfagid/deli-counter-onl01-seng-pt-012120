67a581788fa201103ada5b641e90ef866924993d
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-02 00:28:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line_method(numinline)
   line_method_array = []
   if numinline.length == 0
-    puts "The line is currently empty."
+    puts "There is nobody in line."
   else
     line_method_array.each.with_index(0) do |name, index|
       line_method_array.push("#{index}. #{name}")
