74894aff206eaf36028827043f52b94705899cec
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-02 00:15:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,8 +18,8 @@ function currentLine(katzDeliLine) {
         output += (i + 1) + ". " + katzDeliLine[i] + ", ";
       }
     output = output.slice(0, -2);
-    return `The line is currently: #{output}`;
-  } else {
+    return `The line is currently: #{output}`; }
+  else {
     return "The line is currently empty.";
   }
 }
