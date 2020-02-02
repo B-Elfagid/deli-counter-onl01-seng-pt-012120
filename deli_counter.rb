77e77dbdb8820631027c8c08dfbc22a528e780d5
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-02-02 00:32:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,16 @@
-def line(katz_deli)
-puts "The line is currently empty." if  katz_deli.count == 0
-x=0
-counter ="The line is currently:" 
-while x < katz_deli.length
- counter+=" #{x+1}.#{katz_deli[x]}"
- x+=1
-end
-puts counter 
-end
-
-def take_a_number(katz_deli, name)
-puts "The line is currently empty." if  katz_deli.count == 0
-  katz_deli << name
-  puts "Welcome,#{name}. You are number #{katz_deli.count} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.count == 0
-    puts "There is nobody waiting to be served!"
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
   else
-   puts "Currently serving #{katz_deli[0]}."
-   katz_deli.shift
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
